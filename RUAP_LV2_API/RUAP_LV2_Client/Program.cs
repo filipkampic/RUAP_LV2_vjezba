131c79eb4f5d55301b7ae82bd1705c6dd24d9d51
Filip Kampić <filipkampic1@gmail.com>
2025-12-14 12:46:09 +0100

    Task 2: Consume GET products API from client

--- RUAP_LV2_API/RUAP_LV2_Client/Program.cs
+++ RUAP_LV2_API/RUAP_LV2_Client/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RUAP_LV2_API.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+var client = new HttpClient();
+client.BaseAddress = new Uri("https://localhost:7007/");
+
+var products = await client.GetFromJsonAsync<List<Product>>("api/products");
+
+foreach (var product in products)
+{
+    Console.WriteLine($"{product.Id} - {product.Name} ({product.Category}) {product.Price}");
+}
