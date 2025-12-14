3d0b750386cb22795a222d19abd74f85b39107c7
Filip Kampić <filipkampic1@gmail.com>
2025-12-14 10:44:11 +0100

    Task 1: Add POST method to Products API

--- RUAP_LV2_API/RUAP_LV2_API/Controllers/ProductsController.cs
+++ RUAP_LV2_API/RUAP_LV2_API/Controllers/ProductsController.cs
@@ -8,7 +8,7 @@ namespace RUAP_LV2_API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        Product[] products = new Product[]
+        private static List<Product> products = new List<Product>
         {
             new Product { Id = 1, Name = "Laptop", Category = "IT", Price = 5000 },
             new Product { Id = 2, Name = "Mouse", Category = "IT", Price = 150 }
@@ -30,6 +30,17 @@ namespace RUAP_LV2_API.Controllers
             }
 
             return Ok(product);
+        }
+
+        [HttpPost]
+        public ActionResult<Product> CreateProduct(Product product)
+        {
+            product.Id = products.Max(p => p.Id) + 1;
+            products.Add(product);
+
+            return CreatedAtAction(nameof(GetProduct),
+                new { id = product.Id },
+                product);
         }
     }
 }
