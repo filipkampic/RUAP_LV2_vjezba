2ff2289260f74e4b8f908d269350e8525669df50
Filip Kampić <filipkampic1@gmail.com>
2025-12-14 12:48:12 +0100

    Task 3: Consume POST products API from client

--- RUAP_LV2_API/RUAP_LV2_Client/Program.cs
+++ RUAP_LV2_API/RUAP_LV2_Client/Program.cs
@@ -11,3 +11,17 @@ foreach (var product in products)
 {
     Console.WriteLine($"{product.Id} - {product.Name} ({product.Category}) {product.Price}");
 }
+
+var newProduct = new Product
+{
+    Name = "Monitor",
+    Category = "IT",
+    Price = 1200
+};
+
+var response = await client.PostAsJsonAsync("api/products", newProduct);
+
+if (response.IsSuccessStatusCode)
+{
+    Console.WriteLine("Product successfully created.");
+}
